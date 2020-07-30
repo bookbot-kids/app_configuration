9832de06bd8cbf12b24aca74edb7187a887ecd0f
duc <duc@appiphany.com.au>
2020-07-30 22:15:09 +0700

    Add error handler functions

--- lib/app_configuration.dart
+++ lib/app_configuration.dart
@@ -1,8 +1,10 @@
 library app_configuration;
 
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:cloud_logger/cloud_logger.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:logger/logger.dart';
 import 'package:flutter/foundation.dart' as Foundation;
@@ -18,6 +20,9 @@ class AppConfiguration {
 
   /// Shared preferences to save data https://pub.dev/packages/shared_preferences
   SharedPreferences prefs;
+
+  /// Azure monitor logger output
+  AzureMonitorOutput _azureOutput;
 
   /// All global variable and configs are setup here
   ///
@@ -35,9 +40,10 @@ class AppConfiguration {
 
     prefs = await SharedPreferences.getInstance();
     var outputs = List<LogOutput>();
+    _azureOutput = AzureMonitorOutput(configurations);
     if (Foundation.kReleaseMode) {
       // log to cloud on release
-      outputs.add(AzureMonitorOutput(configurations));
+      outputs.add(_azureOutput);
 
       // In some case, we need to print the console in release as well (e.g. checking log on web release).
       // Let's set `devReleaseMode = true` to turn on the console log
@@ -54,6 +60,11 @@ class AppConfiguration {
         output: MultipleOutput(outputs));
   }
 
+  /// Send all the cached logs into azure monitor
+  Future<void> sendLogsToAzure() async {
+    await _azureOutput.sendAllLogs();
+  }
+
   /// Add new configurations into current
   void append(Map<String, dynamic> configs) {
     configurations.addAll(configs);
@@ -64,5 +75,22 @@ class AppConfiguration {
     String data = await rootBundle.loadString(asset);
     var jsonResult = json.decode(data);
     append(jsonResult);
+  }
+
+  /// Record a flutter
+  Future<void> recordFlutterError(FlutterErrorDetails details) async {
+    logger?.wtf('runZoned error ${details.exception}', details.exception,
+        details.stack);
+  }
+
+  /// Execute a function in [runZoned] and handle error logging
+  ///
+  /// [runZoned]:(https://api.flutter.dev/flutter/dart-async/runZoned.html)
+  void executeInZoned(Function func) {
+    runZoned(() {
+      func();
+    }, onError: (e, stackTrace) {
+      logger?.wtf('runZoned error $e', e, stackTrace);
+    });
   }
 }
